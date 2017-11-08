725e58c20954d4e9ebba32a17bb4b4709b11e8c8
DancingQuanta <andytheseeker@gmail.com>
2017-11-08 21:03:12 +0000

    Added git shell configuration with aliases

---
+++ .config/shell/config-local.d/git.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+## DancingQuanta/git-config - https://github.com/DancingQuanta/git-config
+## git.sh
+## Git shell configuration
+
+# Alias the `git` command to the `hub` command.
+# http://github.com/github/hub
+[ -x $(command -v hub) ] && eval "$(hub alias -s)"
+
+# Alias
+
+alias gb='git branch'
+alias gba='git branch -a'
+alias gstat='git status'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gd='git diff | mate'
+alias gl='git pull'
+alias gp='git push'
+alias glog="git log \
+  --all \
+  --graph \
+  --pretty=format:'%Cred%h%Creset -%C(magenta)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' \
+  --abbrev-commit \
+  --date=relative"
+
